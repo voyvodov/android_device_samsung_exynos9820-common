c870f1d961a5eae283f7fe2550564bd862107e76
Jubscleisson <fcodl0706@gmail.com>
2023-01-12 18:08:16 -0300

    exynos9820-common Build AOSP Recovery
    Signed-off-by: Jubscleisson <fcodl0706@gmail.com>

--- common.mk
+++ common.mk
@@ -262,6 +262,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/linker/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
+# Recovery
+TARGET_USES_AOSP_RECOVERY := true
+
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.4.vendor:64 \
